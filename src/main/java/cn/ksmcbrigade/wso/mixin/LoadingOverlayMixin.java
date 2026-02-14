a3262b85f48d85513c8d24eb9f1500693d02744c
yiyuyan <179544143@qq.com>
2026-02-14 17:12:03 +0800

    UPDATE 1.21.3

--- src/main/java/cn/ksmcbrigade/wso/mixin/LoadingOverlayMixin.java
+++ src/main/java/cn/ksmcbrigade/wso/mixin/LoadingOverlayMixin.java
@@ -2,6 +2,7 @@ package cn.ksmcbrigade.wso.mixin;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.SplashOverlay;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -20,7 +21,7 @@ public class LoadingOverlayMixin {
     @Unique
     private int now = 1;
 
-    @Inject(method = "render",at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;enableDepthTest()V",shift = At.Shift.AFTER))
+    @Inject(method = "render",at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIFFIIIIIII)V",shift = At.Shift.AFTER))
     public void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci){
         context.fill(0,0,context.getScaledWindowWidth(),context.getScaledWindowHeight(),Color.BLACK.getRGB());
 
@@ -30,7 +31,7 @@ public class LoadingOverlayMixin {
         int width = 96;
         int height = 96;
 
-        context.drawTexture(Identifier.of(MOD_ID,"gui/icon.png"),(SWidth-width)/2,(SHeight-height)/2-height/2,0,0,width,height,width,height);
+        context.drawTexture((i)-> RenderLayer.getGuiTextured(Identifier.of(MOD_ID,"gui/icon.png")),Identifier.of(MOD_ID,"gui/icon.png"),(SWidth-width)/2,(SHeight-height)/2-height/2,0,0,width,height,width,height);
     }
 
     @Inject(method = "renderProgressBar",at = @At("HEAD"),cancellable = true)
@@ -42,7 +43,7 @@ public class LoadingOverlayMixin {
         int width = 32;
         int height = 32;
 
-        context.drawTexture(Identifier.of(MOD_ID,"gui/loading/"+now+".png"),(SWidth-width)/2,(SHeight-height)/2+2*height,0,0,width,height,width,height);
+        context.drawTexture((i)->RenderLayer.getGuiTextured(Identifier.of(MOD_ID,"gui/loading/"+now+".png")),Identifier.of(MOD_ID,"gui/loading/"+now+".png"),(SWidth-width)/2,(SHeight-height)/2+2*height,0,0,width,height,width,height);
 
         now++;
 
